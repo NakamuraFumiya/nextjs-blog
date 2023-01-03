6fe029fb96eaaeee42e7eb3fc05e5bf13884329c
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-01-03 19:02:07 +0900

    プロフィールに文章を追加

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,72 @@
-import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import styled from "styled-components";
+
 import { ProfileImage } from '../components/profile';
 import { HeadComponent } from '../components/head';
+
+const BackgroundColor = styled.div`
+  background-color: black;
+`;
+
+const ProfileImg = styled.div`
+  margin-top: 8%;
+  margin-left: 8%;
+`;
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const Body = styled.div`
+  display: flex;
+`;
+
+const SideBody = styled.div`
+  flex-grow: 0.25;
+`;
+
+const MainBody = styled.div`
+  flex-grow: 0.75;
+`;
+
+const WhiteBoldSideText = styled.p`
+  color: white;
+  font-family: "Mukta Mahee";
+  font-size: 120%;
+  font-weight: bold;
+  margin-top: 1%;
+  margin-left: 8%;
+`;
+
+const WhiteNormalSideText = styled.p`
+  color: white;
+  font-family: "Mukta Mahee";
+  font-size: 80%;
+  font-weight: normal;
+  margin-left: 8%;
+  line-height: 0.5;
+`;
+
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <HeadComponent />
 
-      <main>
-        <ProfileImage />
+      <BackgroundColor>
+        <Body>
+          <SideBody>
+            <ProfileImg>
+              <ProfileImage />
+            </ProfileImg>
+            <WhiteBoldSideText>Fumiya Nakamura</WhiteBoldSideText>
+            <WhiteNormalSideText>I’m a back-end developer.</WhiteNormalSideText>
+            <WhiteNormalSideText>Born in 1996.</WhiteNormalSideText>
+          </SideBody>
+          <MainBody>
 
-        <h1 className={styles.title}>
-          Read <Link href="/posts/first-post">this page!</Link>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+          </MainBody>
+        </Body>
+      </BackgroundColor>
+    </>
   )
 }
