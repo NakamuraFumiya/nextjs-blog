42a02314f3957c8c27d9ddf7324b752cb88642de
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-01-04 13:30:04 +0900

    header部分をリファクタ

---
+++ components/header.js
@@ -0,0 +1,89 @@
+import Link from 'next/link';
+import styled from "styled-components";
+
+import { ProfileImage } from './profile';
+import { GithubIconImage } from './github_icon';
+import { TwitterIconImage } from './twitter_icon';
+
+const HeaderWrapper = styled.div`
+  @media screen and (max-width: 767px) {
+    margin-top: 10%;
+    margin-left: 35%;
+  }
+  @media screen and (min-width: 768px) {
+    position: fixed;
+    top: 0;
+    margin-top: 4%;
+    margin-left: 4%;
+  }
+`;
+
+const ProfileImg = styled.div`
+  margin-top: 8%;
+  margin-left: 8%;
+`;
+
+const WhiteBoldHeaderText = styled.p`
+  color: white;
+  font-family: "Mukta Mahee";
+  font-size: 120%;
+  font-weight: bold;
+  margin-top: 3%;
+  letter-spacing: 0.1em;
+`;
+
+const WhiteNormalHeaderText = styled.p`
+  color: white;
+  font-family: "Mukta Mahee";
+  font-size: 80%;
+  font-weight: normal;
+  line-height: 0.5;
+  margin-left: 3%;
+  letter-spacing: 0.1em;
+`;
+
+const IconParent = styled.div`
+  display: flex;
+  column-gap: 6%;
+  margin-top: 12%;
+`;
+
+const IconHover = styled.div`
+&:hover {
+  opacity: 0.5;
+  transition: 0.4s;
+}
+`;
+
+const HeaderBodyDiv = styled.div`
+  @media screen and (min-width: 768px) {
+    padding-left: 30%;
+    background-color: green;
+  }
+`;
+
+export const Header = () => (
+  <>
+    <HeaderWrapper>
+      <ProfileImg>
+        <ProfileImage />
+      </ProfileImg>
+      <WhiteBoldHeaderText>Fumiya Nakamura</WhiteBoldHeaderText>
+      <WhiteNormalHeaderText>I’m a back-end developer.</WhiteNormalHeaderText>
+      <WhiteNormalHeaderText>Born in 1996.</WhiteNormalHeaderText>
+      <IconParent>
+          <Link href="https://twitter.com/NakamuraFumiya3" target="_blank">
+            <IconHover>
+              <TwitterIconImage />
+            </IconHover>
+          </Link>
+          <Link href="https://github.com/NakamuraFumiya" target="_blank">
+            <IconHover>
+              <GithubIconImage />
+            </IconHover>
+          </Link>
+      </IconParent>
+    </HeaderWrapper>
+    <HeaderBodyDiv></HeaderBodyDiv>
+  </>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Link from 'next/link';
 import styled from "styled-components";
 
-import { ProfileImage } from '../components/profile';
-import { GithubIconImage } from '../components/github_icon';
-import { TwitterIconImage } from '../components/twitter_icon';
+import { Header } from '../components/header';
 import { HeadComponent } from '../components/head';
 
-// サイドメニューのCSS
 const BackgroundColor = styled.div`
   background-color: black;
   width: 100%;
@@ -17,47 +14,10 @@ const Body = styled.div`
   display: flex;
 `;
 
-const Header = styled.div`
-  @media screen and (max-width: 767px) {
-    margin-top: 10%;
-    margin-left: 35%;
-  }
-  @media screen and (min-width: 768px) {
-    position: fixed;
-    top: 0;
-    margin-top: 4%;
-    margin-left: 4%;
-  }
-`;
-
-const ProfileImg = styled.div`
-  margin-top: 8%;
-  margin-left: 8%;
-`;
-
 const MainBody = styled.div`
   @media screen and (max-width: 767px) {
     padding-top: 60%;
   }
-`;
-
-const WhiteBoldSideText = styled.p`
-  color: white;
-  font-family: "Mukta Mahee";
-  font-size: 120%;
-  font-weight: bold;
-  margin-top: 3%;
-  letter-spacing: 0.1em;
-`;
-
-const WhiteNormalSideText = styled.p`
-  color: white;
-  font-family: "Mukta Mahee";
-  font-size: 80%;
-  font-weight: normal;
-  line-height: 0.5;
-  margin-left: 3%;
-  letter-spacing: 0.1em;
 `;
 
 const WhiteNormalMainText = styled.p`
@@ -71,63 +31,15 @@ const WhiteNormalMainText = styled.p`
   margin-bottom: 10000px;
 `;
 
-const IconParent = styled.div`
-  display: flex;
-  column-gap: 6%;
-  margin-top: 12%;
-`;
-
-const IconHover = styled.div`
-&:hover {
-  opacity: 0.5;
-  transition: 0.4s;
-}
-`;
-
-// 使ってない
-const Center = styled.div`
-  text-align: center;
-`;
-
-const SideBodyDiv = styled.div`
-  @media screen and (min-width: 768px) {
-    padding-left: 30%;
-    background-color: green;
-  }
-`;
-
 export default function Home() {
   return (
     <>
       <HeadComponent />
 
       <BackgroundColor>
-        <Body>
+        <Body>          
+          <Header />
           
-          {/* サイドBody：要リファクタ */}
-          <Header>
-            <ProfileImg>
-              <ProfileImage />
-            </ProfileImg>
-            <WhiteBoldSideText>Fumiya Nakamura</WhiteBoldSideText>
-            <WhiteNormalSideText>I’m a back-end developer.</WhiteNormalSideText>
-            <WhiteNormalSideText>Born in 1996.</WhiteNormalSideText>
-            <IconParent>
-                <Link href="https://twitter.com/NakamuraFumiya3" target="_blank">
-                  <IconHover>
-                    <TwitterIconImage />
-                  </IconHover>
-                </Link>
-                <Link href="https://github.com/NakamuraFumiya" target="_blank">
-                  <IconHover>
-                    <GithubIconImage />
-                  </IconHover>
-                </Link>
-            </IconParent>
-          </Header>
-          <SideBodyDiv></SideBodyDiv>
-          
-
           {/* メインBody：要リファクタ */}
           <MainBody>
             <WhiteNormalMainText>aaa</WhiteNormalMainText>
