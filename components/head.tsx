00b751408651d2e8d7e9d643b0605afbbb500152
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-05 21:04:52 +0900

    Twitterカードの設定を修正

--- components/head.tsx
+++ components/head.tsx
@@ -6,16 +6,23 @@ export const HeadComponent = () => (
       <title>Fumiya Nakamura</title>
       <link rel="icon" href="/favicon.ico" />
       <meta name="description" content="This is Fumiya Nakamura's portfolio site." />
-      <link rel="canonical" href="https://fumiyanakamura.vercel.app/" />
+
+      {/*ogpの設定*/}
       <meta property="og:title" content="Fumiya Nakamura" />
       <meta property="og:description" content="This is Fumiya Nakamura's portfolio site." />
       <meta property="og:site_name" content="Fumiya Nakamura" />
-      <meta property="og:url" content="https://fumiyanakamura.vercel.app/" />
+      <meta property="og:url" content="https://www.fumiyanakamura.com/" />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="ja_JP" />
       <meta property="og:image" content="/images/fumiya-circle.png" />
+
+      {/*Twitterカードの設定*/}
       <meta name="twitter:card" content="summary" />
+      <meta name="twitter:image" content="/images/fumiya-circle.png" />
       <meta name="twitter:site" content="@NakamuraFumiya3" />
+
+      <link rel="canonical" href="https://www.fumiyanakamura.com/" />
+      <meta name="robots" content="noindex,nofollow" />
     </Head>
   </>
 );
