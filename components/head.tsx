b31bad3f396d66abdac5c4705b2f07fff0e3cebc
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-05 19:52:48 +0900

    metaタグを追加

--- components/head.tsx
+++ components/head.tsx
@@ -5,6 +5,17 @@ export const HeadComponent = () => (
     <Head>
       <title>Fumiya Nakamura</title>
       <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="This is Fumiya Nakamura's portfolio site." />
+      <link rel="canonical" href="https://fumiyanakamura.vercel.app/" />
+      <meta property="og:title" content="Fumiya Nakamura" />
+      <meta property="og:description" content="This is Fumiya Nakamura's portfolio site." />
+      <meta property="og:site_name" content="Fumiya Nakamura" />
+      <meta property="og:url" content="https://fumiyanakamura.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="ja_JP" />
+      <meta property="og:image" content="/images/fumiya-circle.png" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@NakamuraFumiya3" />
     </Head>
   </>
 );
