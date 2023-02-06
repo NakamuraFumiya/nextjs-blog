dbb88d6c6dad9195b627d146d4503a64c56b394e
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-06 23:13:40 +0900

    components配下にpageディレクトリを追加

---
+++ components/page/Top/Top.tsx
@@ -0,0 +1,11 @@
+import { HeadComponent } from '../../head';
+import { BodyComponent } from '../../body';
+
+export const Top = () => {
+  return (
+    <>
+      <HeadComponent />
+      <BodyComponent />
+    </>
+  )
+}
