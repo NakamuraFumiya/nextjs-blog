7f1f269aab63c189e7a807c18fc397aa6777f13d
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-09 00:52:50 +0900

    PageWithSideBar.tsxのstyleをrem指定に変更

--- components/ui/Layout/SideBar/PageWithSideBar.tsx
+++ components/ui/Layout/SideBar/PageWithSideBar.tsx
@@ -1,60 +1,64 @@
 import Link from 'next/link';
 import styled from "styled-components";
-
 import { ProfileIcon } from '../../Icon/ProfileIcon';
 import { GithubIcon } from '../../Icon/GithubIcon';
 import { TwitterIcon } from '../../Icon/TwitterIcon';
 
 const SideBarWrapper = styled.div`
   @media screen and (max-width: 767px) {
-    margin-top: 20%;
-    margin-left: 25%;
+    margin-top: 5rem;
+    margin-left: 6rem;
   }
   @media screen and (min-width: 768px) {
     position: fixed;
-    top: 0;
-    margin-top: 4%;
-    margin-left: 4%;
+    margin-top: 3.6rem;
+    margin-left: 3.6rem;
   }
 `;
 
 const ProfileIconWrapper = styled.div`
   @media screen and (max-width: 767px) {
-    margin-top: 8%;
-    margin-left: 8%;
-    margin-bottom: 2%;
+    margin-top: 1.5rem;
+    margin-left: 1.5rem;
+    margin-bottom: 0.3rem;
   }
   @media screen and (min-width: 768px) {
-    margin-top: 8%;
-    margin-left: 12%;
-    margin-bottom: 10%;
+    margin-top: 1.2rem;
+    margin-left: 1.8rem;
+    margin-bottom: 1.5rem;
   }
 `;
 
-const H1 = styled.h1`
-  color: white;
-  font-size: 1.5rem;
-  margin-top: 1rem;
-  letter-spacing: 0.2rem;
+const ProfileName = styled.h1`
+  @media screen and (max-width: 767px) {
+    color: white;
+    font-size: 1.5rem;
+    margin-top: 1rem;
+    letter-spacing: 0.2rem;
+  }
+  @media screen and (min-width: 768px) {
+    color: white;
+    font-size: 1.5rem;
+    margin-top: 1rem;
+    letter-spacing: 0.2rem;
+  }
 `;
 
-const WhiteNormalHeaderText = styled.p`
+const ProfileDescription = styled.p`
   color: white;
-  font-size: 100%;
-  font-weight: normal;
-  line-height: 0.5;
-  margin-left: 1%;
-  letter-spacing: 0.1em;
+  line-height: 0.6rem;
+  margin-left: 0.2rem;
+  letter-spacing: 0.13rem;
 `;
 
 const IconParent = styled.div`
   display: flex;
-  column-gap: 6%;
+  column-gap: 1rem;
   @media screen and (max-width: 767px) {
-    margin-top: 8%;
+    margin-top: 1.5rem;
   }
   @media screen and (min-width: 768px) {
-    margin-top: 12%;
+    margin-top: 1.8rem;
   }
 `;
 
@@ -67,7 +71,7 @@ const IconHover = styled.div`
 
 const SideBarSpacer = styled.div`
   @media screen and (min-width: 768px) {
-    padding-left: 30%;
+    padding-left: 27rem;
   }
 `;
 
@@ -77,9 +81,9 @@ export const PageWithSideBar = () => (
       <ProfileIconWrapper>
         <ProfileIcon x={144} y={144} />
       </ProfileIconWrapper>
-      <H1>Fumiya Nakamura</H1>
-      <WhiteNormalHeaderText>I’m a back-end developer.</WhiteNormalHeaderText>
-      <WhiteNormalHeaderText>Born in 1996.</WhiteNormalHeaderText>
+      <ProfileName>Fumiya Nakamura</ProfileName>
+      <ProfileDescription>I’m a back-end developer.</ProfileDescription>
+      <ProfileDescription>Born in 1996.</ProfileDescription>
       <IconParent>
         <Link href="https://twitter.com/NakamuraFumiya3" target="_blank">
           <IconHover>
