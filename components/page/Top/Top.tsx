cdfee521a176150cedeb6f4a27add38ca4c3c075
NakamuraFumiya <fumiya3691@gmail.com>
2025-06-18 15:56:02 +0900

    update resume

--- components/page/Top/Top.tsx
+++ components/page/Top/Top.tsx
@@ -73,6 +73,30 @@ const H3 = styled.h3`
   }
 `;
 
+const P = styled.p`
+  color: #DDDDDD;
+  line-height: 1.8;
+  font-weight: 300;
+
+  @media screen and (max-width: 767px) {
+    margin: 0 1.5rem;
+    font-size: 0.9rem;
+    letter-spacing: 0.01rem;
+  }
+
+  @media screen and (min-width: 768px) {
+    margin: 1rem 7rem 0;
+    font-size: 1rem;
+    letter-spacing: 0.05rem;
+  }
+
+  @media screen and (min-width: 1920px) {
+    margin: 1rem 22rem 0;
+    font-size: 1.2rem;
+    letter-spacing: 0.07rem;
+  }
+`;
+
 const BodySpan = styled.div`
   @media screen and (max-width: 767px) {
     padding-bottom: 3.6rem;
@@ -123,16 +147,6 @@ export const Top = () => {
         <PageWithSideBar />
         <br />
         <div>
-          <H1>Skills</H1>
-          <H2>{ArrangeWords(skills)}</H2>
-
-          <H1>Learned</H1>
-          <H2>{ArrangeWords(learned)}</H2>
-
-          <H1>Interest/Studying</H1>
-          <H2>{ArrangeWords(studying)}</H2>
-          <H2>{ArrangeWords(interest)}</H2>
-
           <H1>Carrer</H1>
           <H2>Handy Inc. (Jul, 2025 ~ Present)</H2>
           <H3>Development of "Handy進路指導室" using Go.</H3>
@@ -147,6 +161,16 @@ export const Top = () => {
           <H2>NS Solutions Tokyo Inc. (Apr, 2019 ~ Jul, 2020)</H2>
           <H3>Building a CPU utilization monitoring system for host machines using Elasticsearch and Grafana.</H3>
 
+          <H1>Skills</H1>
+          <H2>{ArrangeWords(skills)}</H2>
+
+          <H1>Learned</H1>
+          <H2>{ArrangeWords(learned)}</H2>
+
+          <H1>Interest/Studying</H1>
+          <H2>{ArrangeWords(studying)}</H2>
+          <H2>{ArrangeWords(interest)}</H2>
+
           <H1>Personality</H1>
           <H2>ISFP-A</H2>
 
@@ -156,6 +180,26 @@ export const Top = () => {
           <H1>Education</H1>
           <H2>Functional Materials Engineering</H2>
           <H3>Ehime University</H3>
+
+          <H1>Description</H1>
+          <P>
+              Hello! My name is Fumiya Nakamura. I started my professional experience as a Web Engineer in August 2020. Using a service called MENTA, I was mentored by an active engineer through text-based communication on Slack. After about five months of study, I successfully changed careers and officially started my journey with Ruby.
+          </P>
+          <P>
+              The first language I touched was Ruby, and I loved it for its intuitive nature. After two years of working with Ruby, I became attracted to the high readability of simpler syntax and the safety of static typing, which made me a big fan of Go. Since then, most of my development work has been in Go.
+          </P>
+          <P>
+              Although I was in a science faculty at university, my major was not in an IT field. As I had focused my learning on the Rails framework, I felt I was completely missing foundational knowledge in computer science. So, in my private time, I began studying topics like OS/hardware mechanics, data structures and algorithms, databases, and networking. I feel this has been very useful for my design work and, above all, has greatly improved my troubleshooting skills for inquiries and incidents, leading to a real sense of improvement in my overall problem-solving abilities. I want to continue this study when I have the chance.
+          </P>
+          <P>
+              I am interested in Domain-Driven Design and software architecture, and I have been learning about more maintainable code and logical layer dependencies.
+          </P>
+          <P>
+              From now on, I want to move beyond the backend application layer and step into the infrastructure domain. For instance, I have fixed slow queries through database performance tuning, but I want to have a wider range of solutions, such as distributing traffic at the infrastructure level or scaling out databases horizontally.
+          </P>
+          <P>
+              As an individual, I want to increase the size of problems I can solve by having strengths in both backend and infrastructure. I want to contribute that knowledge back to the team, grow our product through technology, and in turn, make all users happy through our product.
+          </P>
 
           <BodySpan />
         </div>
